0d25a89dd4282655e9100a6ffdc935a814c7f99c
Yves Siegrist <Elektron1c97@gmail.com>
2024-04-08 01:28:38 +0200

    Disable cookies

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module NowayTyler
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.delete ActionDispatch::Cookies
+    config.middleware.delete ActionDispatch::Session::CookieStore
   end
 end
