ed18eb8bc93871999bbe3f947d2c733d4ad3eacb
Yves Siegrist <Elektron1c97@gmail.com>
2024-04-08 14:32:50 +0200

    Layout for accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,7 @@
 class Account < ApplicationRecord
   validates_presence_of %i[role riot_id summoner_id puuid profile_icon_id summoner_level]
+
+  def opgg_url
+    "https://www.op.gg/summoners/euw/#{riot_id.tr('#', '-')}"
+  end
 end
