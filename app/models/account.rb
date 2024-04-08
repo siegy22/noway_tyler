2b3de47311aac16c9fc8158fdf7a53648e20b5ee
Yves Siegrist <Elektron1c97@gmail.com>
2024-04-08 17:47:05 +0200

    Display the winrate and rank

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,26 @@ class Account < ApplicationRecord
   validates_presence_of %i[role riot_id summoner_id puuid profile_icon_id summoner_level]
   has_many :entries
 
+  def winrate
+    entries.ordered.first.winrate
+  end
+
+  def lossrate
+    entries.ordered.first.lossrate
+  end
+
+  def rank_label
+    entries.ordered.first.rank_label
+  end
+
+  def tier
+    entries.ordered.first.tier
+  end
+
+  def lp
+    entries.ordered.first.league_points
+  end
+
   def opgg_url
     "https://www.op.gg/summoners/euw/#{riot_id.tr('#', '-')}"
   end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,22 @@
 class Entry < ApplicationRecord
+  RANKLESS_TIERS = %w[master grandmaster challenger].freeze
   belongs_to :account
 
   validates_presence_of %i[account_id]
+
+  scope :ordered, -> { order(created_at: :desc) }
+
+  def winrate
+    (wins / (wins.to_f + losses.to_f) * 100).round
+  end
+
+  def lossrate
+    (losses / (wins.to_f + losses.to_f) * 100).round
+  end
+
+  def rank_label
+    return tier.capitalize if RANKLESS_TIERS.include?(tier.downcase)
+
+    "#{tier.capitalize} #{rank}"
+  end
 end
